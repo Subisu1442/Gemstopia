7efdc24e71a858c538cef3d73751d124f601852e
Subisu0017 <Subisu1442@users.noreply.github.com>
2020-09-07 23:58:11 +0700

    Update server.js

--- server.js
+++ server.js
@@ -364,8 +364,8 @@ bot.on('messageReactionAdd', async (reaction, user ) => {
     if (reaction.emoji.name === '👶') await reaction.message.guild.members.cache.get(user.id).roles.add('744036349157572718')
     if (reaction.emoji.name === '👦') await reaction.message.guild.members.cache.get(user.id).roles.add('744036453897601035')
     if (reaction.emoji.id === '👴') await reaction.message.guild.members.cache.get(user.id).roles.add('744036520113078352')
-    if (reaction.emoji.name === '👧') await reaction.message.guild.members.cache.get(user.id).roles.add('744033796835377172')
-    if (reaction.emoji.id === '👦') await reaction.message.guild.members.cache.get(user.id).roles.add('744033864078327888')
+    if (reaction.emoji.name === '👧') await reaction.message.guild.members.cache.get(user.id).roles.add('744033864078327888')
+    if (reaction.emoji.id === '👦') await reaction.message.guild.members.cache.get(user.id).roles.add('744033796835377172')
   }
 });
 
@@ -380,8 +380,8 @@ bot.on('messageReactionRemove', async (reaction, user ) => {
     if (reaction.emoji.name === '👶') await reaction.message.guild.members.cache.get(user.id).roles.remove('744036349157572718')
     if (reaction.emoji.name === '👦') await reaction.message.guild.members.cache.get(user.id).roles.remove('744036453897601035')
     if (reaction.emoji.id === '👴') await reaction.message.guild.members.cache.get(user.id).roles.remove('744036520113078352')
-    if (reaction.emoji.name === '👧') await reaction.message.guild.members.cache.get(user.id).roles.remove('744033796835377172')
-    if (reaction.emoji.id === '👦') await reaction.message.guild.members.cache.get(user.id).roles.remove('744033864078327888')
+    if (reaction.emoji.name === '👧') await reaction.message.guild.members.cache.get(user.id).roles.remove('744033864078327888')
+    if (reaction.emoji.id === '👦') await reaction.message.guild.members.cache.get(user.id).roles.remove('744033796835377172')
   }
 });
 bot.login(process.env.token);
